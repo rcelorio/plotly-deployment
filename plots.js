1fdf93736ecd48e5dcd720d51cc623b16b50c160
Raul Celorio <rfcelorio@Rauls-MacBook-Pro.local>
2020-03-31 13:52:43 -0700

    working through module

--- plots.js
+++ plots.js
@@ -19,7 +19,7 @@ function init() {
           .text(sample)
           .property("value", sample);
         });
-
+        buildMetadata(d3.select("#selDataset").node().value);
     })
 }
 
@@ -51,4 +51,3 @@ function optionChanged(newSample) {
   init();
  
   
-  buildMetadata(d3.select("#selDataset").node().value);
