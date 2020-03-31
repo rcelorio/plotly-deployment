8c466043061b1740846dd4defe19b5b77dab7dd5
Raul Celorio <rfcelorio@Rauls-MBP.lan>
2020-03-31 09:54:19 -0700

    working through the module

--- plots.js
+++ plots.js
@@ -11,5 +11,36 @@ function init() {
           .property("value", sample);
       });
   })}
+
+  // select control
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    //buildCharts(newSample);
+  }
+
+  // build panel metadata
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+ 
+      PANEL.append("h6").text("ID: " + result.id);
+      PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
+      PANEL.append("h6").text("GENDER: " + result.gender);
+      PANEL.append("h6").text("AGE: " + result.age);
+      PANEL.append("h6").text("LOCATION: " + result.location);
+      PANEL.append("h6").text("BBTYPE: " + result.bbtype);
+      PANEL.append("h6").text("WFREQ: " + result.wfreq);
+     
+     
+    });
+  }
+
+
+
   
   init();
